45ff78c2d3391bb32031d0215ec2fb52243aac02
iPad Guy <ipad.guy.100@gmail.com>
2022-10-24 16:37:27 -0400

    Database is created

--- src/musichistory/models.py
+++ src/musichistory/models.py
@@ -14,12 +14,27 @@ schema = Config.DB_SCHEMA
 Base = declarative_base(metadata=sa.MetaData(schema=schema))
 
 
+class PlayInfo(Base):
+	__table__ = sa.Table(
+		"td_playinfo", Base.metadata,
+		sa.Column("id", sa.BigInteger, sa.Identity(always=True), primary_key=True),
+		sa.Column("timestamp", sa.Float, nullable=False, unique=True),
+		sa.Column("fi_id", sa.BigInteger),
+		sa.Column("pathname", sa.String(1024), index=True, nullable=False),
+		sa.Column('asof', sa.DateTime(timezone=True), index=True, nullable=False, server_default=sa.func.now()),
+		schema=schema,
+	)
+
+	def __repr__(self):
+		return f"<PlayInfo: id: {self.id} {self.pathname} {self.asof}>"
+
+
 class FileInfo(Base):
 	__table__ = sa.Table(
 		"dt_fileinfo",
 		Base.metadata,
 		sa.Column("id", sa.BigInteger, sa.Identity(start=100001, always=True), primary_key=True),
-		sa.Column("pathname", sa.String(1024), unique=True, nullable=False),
+		sa.Column("pathname", sa.String(1024), nullable=False, unique=False),
 		sa.Column('asof', sa.DateTime(timezone=True), index=True, nullable=False, server_default=sa.func.now()),
 		schema=schema,
 	)
