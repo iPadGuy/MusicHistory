1279e7b89c74d2f8deb119b9c387eb839ee94a83
iPad Guy <ipad.guy.100@gmail.com>
2022-12-19 08:36:37 -0500

    Improved playlist creation

--- stackoverflow_1.sql
+++
@@ -1,8 +0,0 @@
-DO $$
-  declare
-    result record;
-BEGIN
-    FOR result IN Select * FROM (VALUES ('one'), ('two'), ('three')) AS t (path) LOOP 
-        RAISE NOTICE 'path: ~/Desktop/%.csv', result.path;
-    END LOOP;
-END; $$;
